0d88f3534555e1137bad5c50259919d07f0eb41e
Aziz Nurul Hidayat <47619109+aziznurul@users.noreply.github.com>
2023-11-23 10:52:58 +0700

    Q7.js

--- js/Q7.js
+++ js/Q7.js
@@ -1,1 +1,40 @@
+$(function () {
+  $(".place").click(function () {
+    $(this).toggleClass("aktif");
+  });
+  $(".unbooked").attr("title","Unbooked");
+  $(".taken").attr("title","Taken");
+});
 
+$("#submit").click(function () {
+  $(".place").each(function () {
+    if ($(this).hasClass("aktif")) {
+      $(this).addClass("taken");
+      $(this).removeClass("unbooked");
+      $(this).attr("title","Taken");
+      alert("Seats booked: " + $(this).text())
+    }
+  });
+});
+
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.getElementById('count')
+
+container.addEventListener('click', function (e) {
+  console.log(e.target)
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    console.log(e.target)
+    e.target.classList.toggle('selected')
+    updateSelectedCount()
+  }
+})
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected')
+  const selectedSeatsCount = selectedSeats.length
+  count.innerText = selectedSeatsCount
+}
